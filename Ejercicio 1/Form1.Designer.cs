8c6c35f2cc020b7d99366dd4dc495db8e608ae01
geovafuentes22 <geofuentes.gf@gmail.com>
2021-02-14 23:17:03 -0600

    Subiendo ejercicios guia 5

---
+++ Ejercicio 1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtsalario = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.combo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(532, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(555, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CALCULANDO SALARIOS:";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(138, 150);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(120, 22);
+            this.txtnombre.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(402, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Salario:";
+            // 
+            // txtsalario
+            // 
+            this.txtsalario.Location = new System.Drawing.Point(477, 149);
+            this.txtsalario.Name = "txtsalario";
+            this.txtsalario.Size = new System.Drawing.Size(128, 22);
+            this.txtsalario.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 39);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // combo
+            // 
+            this.combo.FormattingEnabled = true;
+            this.combo.Location = new System.Drawing.Point(259, 261);
+            this.combo.Name = "combo";
+            this.combo.Size = new System.Drawing.Size(179, 24);
+            this.combo.TabIndex = 13;
+            this.combo.SelectedIndexChanged += new System.EventHandler(this.combo_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(688, 539);
+            this.Controls.Add(this.combo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsalario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fecha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtsalario;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox combo;
+    }
+}
+
---
+++ Ejercicio 1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            label1.Text = DateTime.Now.ToLongDateString();
+            combo.Items.Clear();
+            combo.Items.Add("Gerente");
+            combo.Items.Add("SubGerente");
+            combo.Items.Add("Secretaria");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre = txtnombre.Text;
+            string combito = combo.Text;
+            double salario = double.Parse(txtsalario.Text);
+            double descuento = 0.20;
+            double salarioFinal = 0;
+
+            switch(combito)
+            {
+                case "Gerente":
+                    descuento = salario * 0.20;
+                    salarioFinal = salario - descuento;
+                    break;
+                case "SubGerente":
+                    descuento = salario * 0.15;
+                    salarioFinal = salario - descuento;
+
+                    break;
+                case "Secretaria":
+                    descuento = salario * 0.05;
+                    salarioFinal = salario - descuento;
+                    break;
+            }
+
+            MessageBox.Show("Amigo " + nombre + " su salario aplicando descuentos será de: " +" $ " +salarioFinal + " Dolares");
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
